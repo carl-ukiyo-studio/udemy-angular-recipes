10848eba07486b2d613cb868524bcea8ce3f7f2d
Carl Meyer <carl@ukiyo-studio.co.uk>
2022-12-07 09:24:04 +0000

    Basic Components Done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is simply a test', 'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528')
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -2,7 +2,7 @@ export class Recipe {
   name: string;
   description: string;
   imgPath: string;
-  
+
   constructor(name: string, description: string, imgPath: string) {
     this.name = name;
     this.description = description;
